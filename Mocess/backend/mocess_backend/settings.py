08cbbbf962357870bcd3c500104777b424c9576d
BelemsigriWindyamJusteYannel <windyamjusteyannel.belemsigri@usmba.ac.ma>
2025-08-27 14:25:20 +0100

    Ok

--- Mocess/backend/mocess_backend/settings.py
+++ Mocess/backend/mocess_backend/settings.py
@@ -150,6 +150,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 ]
 
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://.*\.vercel\.app$",
+]
+
+
 CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework settings
