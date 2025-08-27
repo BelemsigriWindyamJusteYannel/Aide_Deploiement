a521ec597ecb65a6c1ec7dd08696d927a249c9a0
BelemsigriWindyamJusteYannel <windyamjusteyannel.belemsigri@usmba.ac.ma>
2025-08-27 16:52:02 +0100

    Ok

--- Mocess/backend/mocess_backend/settings.py
+++ Mocess/backend/mocess_backend/settings.py
@@ -144,7 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
-    ".vercel.app",
+    "https://aide-deploiement.vercel.app/",
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
