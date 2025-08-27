45b49c4faaf720e093c5794cfa3ce395b86c6093
BelemsigriWindyamJusteYannel <windyamjusteyannel.belemsigri@usmba.ac.ma>
2025-08-27 16:31:34 +0100

    Ok

--- Mocess/backend/mocess_backend/settings.py
+++ Mocess/backend/mocess_backend/settings.py
@@ -136,13 +136,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "aide-deploiement-5qafddfrx.vercel.app"
+    "*"
 ]
 
 CORS_ALLOWED_ORIGIN_REGEXES = [
